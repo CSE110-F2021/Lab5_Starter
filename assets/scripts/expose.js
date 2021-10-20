00054951b3204de426dd154910e53e04cc7dba09
Hyunjo <hyl130@ucsd.edu>
2021-10-20 16:51:20 -0700

    added setImage

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,16 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+//document.title = "CSE110"; //This line was added by joe for testing purpose
+
+function setImage(event){
+  var hornImage = document.getElementsByTagName('img')[0];
+  hornImage.setAttribute("src", "assets/images/" + event.target.value + ".svg");
+} 
+
 function init() {
   // TODO
+  
+  var hornType = document.getElementById("horn-select");
+  hornType.addEventListener('click', setImage);
 }
