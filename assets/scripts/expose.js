637651bd317fafd2c66e34f06d8655482f83ddb5
Hyunjo <hyl130@ucsd.edu>
2021-10-20 17:44:38 -0700

    audio icon added

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,16 +2,35 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-//document.title = "CSE110"; //This line was added by joe for testing purpose
 
-function setImage(event){
-  var hornImage = document.getElementsByTagName('img')[0];
+function setHorn(event){
+  var hornImage = document.getElementsByTagName("img")[0];
   hornImage.setAttribute("src", "assets/images/" + event.target.value + ".svg");
+
+  var hornAudio = document.getElementsByClassName("hidden")[0];
+  hornAudio.setAttribute("src", "assets/audio/" + event.target.value + ".mp3"); 
 } 
+
+function setVolume(event){
+  var volumeValue = document.getElementById("volume").value;
+  var volumeIcon = document.getElementsByTagName("img")[1];
+  console.log(volumeIcon);
+  if( volumeValue == 0){
+    volumeIcon.setAttribute("src", "assets/icons/volume-level-0.svg");  
+  }
+  else if( volumeValue >= 1 && volumeValue < 33){
+    volumeIcon.setAttribute("src", "assets/icons/volume-level-1.svg");  
+  }
+  else if( volumeValue >= 33 && volumeValue < 67){
+    volumeIcon.setAttribute("src", "assets/icons/volume-level-2.svg");  
+  }
+  else if( volumeValue >= 67){
+    volumeIcon.setAttribute("src", "assets/icons/volume-level-3.svg");  
+  } 
+}
 
 function init() {
   // TODO
-  
-  var hornType = document.getElementById("horn-select");
-  hornType.addEventListener('click', setImage);
+  document.getElementById("horn-select").addEventListener('change', setHorn);
+  document.getElementById("volume-controls").addEventListener('change', setVolume);
 }
