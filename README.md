f329182de9700c4d6e13cddcf824fed7bae25154
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2021-10-22 14:46:58 -0700

    finished explore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Amitesh Sharma
+
+# Explore pt.2 Github Actions
+https://github.com/ASharma2001/github-actions-for-ci/issues
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,5 +4,47 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  console.log('test');
+  const button = document.querySelector('button');
+  const image = document.querySelector('img');
+  const select = document.getElementById('voice-select');
+  const textArea = document.getElementById('text-to-speak');
+  const synth = window.speechSynthesis;
+
+  synth.addEventListener("voiceschanged", () => {
+    const voices = speechSynthesis.getVoices();
+    if(voices.length !== 0) {
+      for(var i = 0; i < voices.length; i++) {
+        const voiceOption = document.createElement('option');
+        voiceOption.textContent = voices[i].name;
+        voiceOption.setAttribute('data-name', voices[i].name);
+        document.getElementById("voice-select").appendChild(voiceOption);
+      }
+    }
+  });
+
+  button.addEventListener('click', (e) => {
+    const voices = synth.getVoices();
+    // if no value, then say default message
+    const text = textArea.value ? textArea.value : "hello, please input some text";
+    const name = select.value;
+    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = findVoice(name, voices);
+    synth.speak(utterance);
+
+    // change to talking image when speaking
+    utterance.onstart = function(event) {
+      console.log('here');
+      image.src = "assets/images/smiling-open.png";
+    }
+
+    // go back to original image
+    utterance.onend = function(event) {
+      image.src = "assets/images/smiling.png";
+    }
+  });
+
+  const findVoice = (name, voices) => (
+    voices.filter(voice => voice.name === name)[0]
+  );
+
 }
