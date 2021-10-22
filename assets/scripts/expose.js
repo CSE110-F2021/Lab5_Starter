39c414b1654a57769091e4745998a81415475f6b
BrandonC9 <brandon.w.chen@gmail.com>
2021-10-21 17:44:47 -0700

    added expose.js skeleton

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,26 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // TODO 
+}
+
+const selectSound = document.querySelector('horn-select');
+selectSound.addEventListener('change', changeSound);
+
+function changeSound() {
+
+}
+
+const soundLevel = document.getElementById('volume-controls');
+soundLevel.addEventListener('change', changeLevel);
+
+function changeLevel() {
+
+}
+
+const playButtonPressed = document.querySelector('button');
+playButtonPressed = addEventListener('click', playSound);
+
+function playSound() {
+
 }
