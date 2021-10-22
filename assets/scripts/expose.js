8489f65bb20008560a1f1cf5e452cfd5a7470ae8
Lavita Zuo <x5zuo@ucsd.edu>
2021-10-22 12:30:16 -0700

    Added image display feature

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,35 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var select = document.getElementById('horn-select');
+  var volume = document.getElementById('volume');
+  const img = document.querySelector('img');
+  const icon = document.getElementById('volume_controls').getElementsByTagName('img');
+  select.addEventListener('change', setHorn);
+  volume.addEventListener('volume', setVolume);
+
+  function setVolume() {
+    var vol = volume.value;
+    if(vol == 0){
+      icon.src = 'assets/icons/volume-level-0.svg'
+    } else if(vol < 33){
+      icon.src = 'assets/icons/volume-level-1.svg'
+    } else if(vol < 67){
+      icon.src = 'assets/icons/volume-level-2'
+    } else{
+      icon.src = 'assets/icons/volume-level-3'
+    }
+  }
+
+  function setHorn() {
+    var choice = select.value;
+    if(choice === 'air-horn'){
+      img.src = 'assets/images/air-horn.svg'
+    } else if (choice === 'car-horn'){
+      img.src = 'assets/images/car-horn.svg'
+    } else  {
+      img.src = 'assets/images/party-horn.svg'
+    }
+  }
+
 }
