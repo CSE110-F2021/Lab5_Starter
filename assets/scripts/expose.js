53c481dddf84a8e9e557eb131bab35cff43d46c6
Faris Ashai <farisashai@gmail.com>
2021-10-22 15:16:33 -0700

    complete expose part

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,48 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const images = document.querySelectorAll('img');
+
+  const hornImage = images[0];
+  const volumeImage = images[1];
+
+  const hornSelector = document.getElementById('horn-select');
+  const volumeControls = document.getElementById('volume');
+
+  const audio = document.querySelector('audio');
+  const playButton = document.querySelector('button')
+
+  const jsConfetti = new JSConfetti()
+  let activeHorn = ''
+
+  const onSelectChange = () => {
+    activeHorn = hornSelector.value;
+
+    hornImage.src = `assets/images/${activeHorn}.svg`;
+    audio.src = `assets/audio/${activeHorn}.mp3`;
+  };
+  hornSelector.addEventListener('change', onSelectChange)
+
+  const onVolumeChange = () => {
+    const value = volumeControls.value;
+
+    const tier = Math.ceil(3*value/100);
+    volumeImage.src = `assets/icons/volume-level-${tier}.svg`
+
+    audio.volume = value / 100;
+  };
+  volumeControls.addEventListener('input', onVolumeChange);
+
+
+  const onPlayClicked = () => {
+    if (activeHorn === 'party-horn') {
+      jsConfetti.addConfetti({
+        emojis: [ '🌸', '🌷', '🌼', '🌻', '🌺', '🏵️'],
+        confettiNumber: 250,
+      })
+      
+    }
+    audio.play()
+  }
+  playButton.addEventListener('click', onPlayClicked);
 }
