1e2c90ff6c04949a57ead3254063311adbaf0637
jjsinger11 <61098535+jjsinger11@users.noreply.github.com>
2021-10-21 17:40:20 -0700

    Testing Air-horn

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,12 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById('horn-select');
 }
+
+const selectElement = document.getElementById('horn-select');
+
+selectElement.addEventListener('change', (event) => {
+  document.querySelector('img').src = "../assets/images/" + "air-horn" + '.svg';
+  console.log(document.querySelector('img').src);
+});
