a4c1bd9b84b8002dbc9025b03f6ee5b6895aa827
Dorothy Chou <dchou@ucsd.edu>
2021-10-22 15:45:03 -0700

    update expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,12 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const selectElement = document.getElementById('horn-select');
+  const volumeInput = document.querySelector('input');
+  let audio;
+  let hornType;
+
+  selectElement.addEventListener('change', (event) => {
+    hornType = event.target.value;
+    document.querySelector('section>img').src = "../assets/images/" + hornType + '.svg';
+    audio = new Audio("../assets/audio/" + hornType + '.mp3');
+  });
+
+  volumeInput.addEventListener('input', updateValue);
+
+  function updateValue(e) {
+    let vol = e.target.value;
+    audio.volume = vol/100.0;
+    if(vol < 1) {
+      document.querySelector('div>img').src = "../assets/icons/volume-level-0.svg";
+    }
+    else if (vol<33) {
+      document.querySelector('div>img').src = "../assets/icons/volume-level-1.svg";
+    }
+    else if (vol<67) {
+      document.querySelector('div>img').src = "../assets/icons/volume-level-2.svg";
+    }
+    else {
+      document.querySelector('div>img').src = "../assets/icons/volume-level-3.svg";
+    }
+  }
+
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', event => {
+    console.log(event.detail);
+    audio.play();
+    if(hornType == 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
 }
 
-const selectElement = document.getElementById('horn-select');
-
-selectElement.addEventListener('change', (event) => {
-  //console.log(event.target.value);
-  document.querySelector('img').src = "../assets/images/" + event.target.value + '.svg';
-});
