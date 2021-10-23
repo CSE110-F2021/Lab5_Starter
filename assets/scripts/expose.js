5830d2a64ced7030686ed1153f16d5dfbb7a6d09
Jieun Lee <45555782+JinnnyWinnny@users.noreply.github.com>
2021-10-23 01:20:22 -0700

    expose finished

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,6 +1,7 @@
 // expose.js
 
 window.addEventListener("DOMContentLoaded", init);
+const jsConfetti = new JSConfetti({ window });
 
 function init() {
   // TODO
@@ -8,36 +9,64 @@ function init() {
 
   optionSelector(imgSrc);
   volumeControl(imgSrc);
+
+  audioPlay();
 }
+
 function optionSelector(imgSrc) {
   const horns = document.getElementById("horn-select");
 
   horns.addEventListener("change", (e) => {
     var val = e.target.value;
-    // var src = e.target.src;
 
     if (val == "air-horn") {
       imgSrc[0].src = "assets/images/air-horn.svg";
-      document.getElementsByClassName("hidden").src =
+      document.getElementsByClassName("hidden")[0].src =
         "assets/audio/air-horn.mp3";
     } else if (val == "car-horn") {
       imgSrc[0].src = "assets/images/car-horn.svg";
-      document.getElementsByClassName("hidden").src =
+      document.getElementsByClassName("hidden")[0].src =
         "assets/audio/car-horn.mp3";
     } else {
       imgSrc[0].src = "assets/images/party-horn.svg";
-      document.getElementsByClassName("hidden").src =
+      document.getElementsByClassName("hidden")[0].src =
         "assets/audio/party-horn.mp3";
     }
   });
 }
-function volumeControl(imgSrc) {
-  var vol = document.getElementById("volume");
-  const input = document.querySelectorAll("input");
-  console.log(`${vol.value}`);
-  // console.log(`${input.value}`);
 
-  if (input.value == 0) {
-    imgSrc[1].src = "assets/icons/volume-level-0.svg";
-  }
+function volumeControl(imgSrc) {
+  const volCont = document.querySelector("#volume-controls");
+  let audio = document.getElementsByClassName("hidden");
+
+  volCont.addEventListener("change", (e) => {
+    let volume = e.target.value;
+    console.log(volume);
+
+    if (volume == 0) {
+      imgSrc[1].src = "assets/icons/volume-level-0.svg";
+    } else if (volume > 1 && volume <= 33) {
+      imgSrc[1].src = "assets/icons/volume-level-1.svg";
+    } else if (volume > 33 && volume < 67) {
+      imgSrc[1].src = "assets/icons/volume-level-2.svg";
+    } else {
+      imgSrc[1].src = "assets/icons/volume-level-3.svg";
+    }
+    audio[0].volume = 0.01 * volume;
+  });
+}
+
+function audioPlay() {
+  const audioBtn = document.querySelector("button");
+  let audio = document.getElementsByClassName("hidden");
+
+  audioBtn.addEventListener("click", (e) => {
+    var optionSrc = document.getElementsByClassName("hidden")[0].src;
+    audio[0].play();
+    console.log(optionSrc);
+    if (optionSrc.includes("assets/audio/party-horn.mp3")) {
+      console.log("working");
+      jsConfetti.addConfetti();
+    }
+  });
 }
