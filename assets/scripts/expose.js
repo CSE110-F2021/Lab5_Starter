c2bc3bd96207a4e6234f2ff50ac02da87bbfdee4
BrandonC9 <brandon.w.chen@gmail.com>
2021-10-23 16:21:21 -0700

    added sound select, volume change, and play functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,26 +3,45 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO 
-}
+  let sound = null;
+  document.querySelector("button + audio").volume = 0.5;
 
-const selectSound = document.querySelector('horn-select');
-selectSound.addEventListener('change', changeSound);
+  document.getElementById("horn-select").addEventListener('input', function() {
+    sound = document.getElementById("horn-select").value;
+    if(sound == 'air-horn') {
+      document.querySelector("header + img").src = "assets/images/air-horn.svg";
+      document.querySelector("button + audio").src = "assets/audio/air-horn.mp3";
+    } else if (sound == 'car-horn') {
+      document.querySelector("header + img").src = "assets/images/car-horn.svg";
+      document.querySelector("button + audio").src = "assets/audio/car-horn.mp3";
+    } else {
+      document.querySelector("header + img").src = "assets/images/party-horn.svg";
+      document.querySelector("button + audio").src = "assets/audio/party-horn.mp3";
+    }
+  });
+  
+  document.getElementById("volume-controls").addEventListener('change', function() {
+    let level = document.getElementById("volume").value;
+    if(level == 0) {
+      document.querySelector("div img").src = "assets/icons/volume-level-0.svg";
+    } else if(level < 32) {
+      document.querySelector("div img").src = "assets/icons/volume-level-1.svg";
+    } else if(level >= 33 && level < 67) {
+      document.querySelector("div img").src = "assets/icons/volume-level-2.svg";
+    } else {
+      document.querySelector("div img").src = "assets/icons/volume-level-3.svg";
+    }
 
-function changeSound() {
-
-}
-
-const soundLevel = document.getElementById('volume-controls');
-soundLevel.addEventListener('change', changeLevel);
-
-function changeLevel() {
-
-}
-
-const playButtonPressed = document.querySelector('button');
-playButtonPressed = addEventListener('click', playSound);
-
-function playSound() {
-
+    document.querySelector("button + audio").volume = level / 100.0;
+  });
+  
+  document.querySelector('button').addEventListener('click', function() {
+    if(sound == null)
+      return;
+    
+    document.querySelector("button + audio").play();
+    if(sound == 'party-horn') {
+      //also do confetti
+    }
+  });
 }
