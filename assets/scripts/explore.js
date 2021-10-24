6de2abe3889deb73282ec152583c7cbd2224a632
BrandonC9 <brandon.w.chen@gmail.com>
2021-10-23 19:15:52 -0700

    added basic speech synthesis to explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,49 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  populateVoiceList();
+  if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  var selectedVoice;
+  document.getElementById("voice-select").addEventListener('change', function() {
+    selectedVoice = document.getElementById("voice-select").value;
+  })
+
+  document.querySelector("select + button").addEventListener('click', function() {
+    if(selectedVoice == null)
+      return;
+    
+    var voices = speechSynthesis.getVoices();
+    var utter = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+    for(let i = 0; i < voices.length; i++) {
+      if(voices[i].name === selectedVoice) {
+        utter.voice = voices[i];
+      }
+    }
+    speechSynthesis.speak(utter);
+  });
+}
+
+//populateVoiceList() from https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  var voices = speechSynthesis.getVoices();
+
+  for(var i = 0; i < voices.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
