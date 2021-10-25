6d0129f7d9062cdc6a39b12319c4a2fae1126c7e
Vivian Dang <vtdang@ucsd.edu>
2021-10-24 21:25:44 -0700

    initial explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,58 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var synth = window.speechSynthesis;
+  var voiceSelect = document.getElementById('voice-select');
+  var inputTxt = document.getElementById('text-to-speak');
+  var button = documentquerySelector('button');
+
+  var voices = [];
+  var image = document.querySelector("img");
+
+  function voiceListPopulator(){
+    voices = synth.getVoices();
+
+    for(var i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+  
+      if(voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  voiceListPopulator();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = voiceListPopulator;
+  }
+
+  button.onClick = function(event) {
+    event.preventDefault();
+  
+    var textToSpeech = new SpeechSynthesisUtterance(inputTxt.value);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        textToSpeech.voice = voices[i];
+      }
+    }
+    speechSynthesis.speak(textToSpeech);
+
+    if(speechSynthesis.speaking){
+      image.src = "/assets/images/smiling-open.png";
+    }
+  
+    textToSpeech.onend = function(event){
+      image.src = "/assets/images/smiling.png";
+    }
+  
+    inputTxt.blur();
+
+  }
+
+
 }
