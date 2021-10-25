74d701e300a1cc624827cdeee9c1fc5863bfe278
Patricia Wu <49573110+PatriciaWuu@users.noreply.github.com>
2021-10-24 23:41:41 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,43 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
+function init(){
+var docuSele = document.getElementById("horn-select");
+var image = document.querySelector("img");
+//var seleImg = document.getElementsByID('here');
+docuSele.addEventListener('change',(event) => {
+  let tagg = event.target.value;
+  if(tagg=='air-horn'){
+    image.src = 'assets/images/air-horn.svg';
+  }
+  else if(tagg=='car-horn'){
+    image.src = 'assets/images/car-horn.svg';
+  }
+  else
+  image.src = 'assets/images/party-horn.svg';
+  ;});
 
-function init() {
-  // TODO
+  var vol = document.getElementById("audio");
+  docuSele.addEventListener('change',(event) => {
+    let volTagg = event.target.value;
+    if(volTagg=='air-horn'){
+      vol.src = 'assets/audio/air-horn.svg';
+    }
+    else if(tagg=='car-horn'){
+      vol.src = 'assets/audio/car-horn.svg';
+    }
+    else
+    vol.src = 'assets/audio/party-horn.svg';
+    ;});
+//const log = document.getElementById('horn-select');
+
+//change audio
+var button = document.querySelector("botton");
+const jsCon = new JSConfetti();
+button.addEventListener('click', (event) => {
+  jsCon.addConfetti();
+})
+
+
+
 }
+
