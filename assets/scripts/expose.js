781ad8b07dcf26b3a29f6c8ddf6f88183d7aeca8
Patricia Wu <49573110+PatriciaWuu@users.noreply.github.com>
2021-10-25 19:01:57 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,31 +2,26 @@ window.addEventListener('DOMContentLoaded', init);
 function init(){
 var docuSele = document.getElementById("horn-select");
 var image = document.querySelector("img");
+var vol = document.querySelector("audio");
+
 //var seleImg = document.getElementsByID('here');
 docuSele.addEventListener('change',(event) => {
   let tagg = event.target.value;
   if(tagg=='air-horn'){
     image.src = 'assets/images/air-horn.svg';
+    vol.src = 'assets/audio/air-horn.mp3';
+
   }
   else if(tagg=='car-horn'){
     image.src = 'assets/images/car-horn.svg';
+    vol.src = 'assets/audio/car-horn.mp3';
+
   }
   else
   image.src = 'assets/images/party-horn.svg';
-  ;});
+  vol.src = 'assets/audio/party-horn.mp3';
 
-  var vol = document.querySelector("audio");
-  docuSele.addEventListener('change',(event) => {
-    let volTagg = event.target.value;
-    if(volTagg=='air-horn'){
-      vol.src = 'assets/audio/air-horn.mp3';
-    }
-    else if(volTagg=='car-horn'){
-      vol.src = 'assets/audio/car-horn.mp3';
-    }
-    else
-    vol.src = 'assets/audio/party-horn.mp3';
-    ;});
+  ;});
 
 //change audio
  var volcon = document.getElementById("volume-controls");
